7cf554632585dd588aaaf58812be17c4885f55fe
Gerard Obey Francisco <127649892+GOFrancisco15@users.noreply.github.com>
2025-09-19 08:30:46 +0800

    Add files via upload

---
+++ laboratory_activity_1.ino
@@ -0,0 +1,19 @@
+int ledPins[] = {8, 9, 10, 11, 12};
+
+void setup() {
+  for (int i = 0; i < 5; i++) {
+    pinMode(ledPins[i], OUTPUT);
+  }
+}
+
+void loop() {
+  for (int i = 0; i <= 4; i++) {
+    digitalWrite(ledPins[i], HIGH);
+    delay(1000);
+  }
+  
+  for (int i = 0; i <= 4; i++) {
+    digitalWrite(ledPins[i], LOW);
+    delay(1000);
+  }
+}
