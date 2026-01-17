c9a33003f3a5b46c0ec6dc6e2189b8286518f794
Gerard Obey Francisco <127649892+GOFrancisco15@users.noreply.github.com>
2026-01-17 22:06:54 +0800

    Update and rename lab8.ino to Lab_7/lab7_activity.ino

--- Lab_7/lab7_activity.ino
+++ Lab_7/lab7_activity.ino
@@ -1,4 +1,5 @@
-// ----- Pins -----
+// Laboratory Activity #7: Controllling Arduino using FastAPI
+
 const int RED_LED = 7;
 const int GREEN_LED = 6;
 const int BLUE_LED = 5;
@@ -7,12 +8,10 @@ const int BTN1 = 12;
 const int BTN2 = 11;
 const int BTN3 = 10;
 
-// LED states
 bool redState = false;
 bool greenState = false;
 bool blueState = false;
 
-// Button previous state for edge detection
 bool lastBtn1 = HIGH;
 bool lastBtn2 = HIGH;
 bool lastBtn3 = HIGH;
@@ -20,33 +19,27 @@ bool lastBtn3 = HIGH;
 void setup() {
   Serial.begin(9600);
 
-  // LEDs
   pinMode(RED_LED, OUTPUT);
   pinMode(GREEN_LED, OUTPUT);
   pinMode(BLUE_LED, OUTPUT);
 
-  // Buttons
   pinMode(BTN1, INPUT_PULLUP);
   pinMode(BTN2, INPUT_PULLUP);
   pinMode(BTN3, INPUT_PULLUP);
 
-  // Initialize LED OFF
   digitalWrite(RED_LED, LOW);
   digitalWrite(GREEN_LED, LOW);
   digitalWrite(BLUE_LED, LOW);
 
-  // Initialize button state
   lastBtn1 = digitalRead(BTN1);
   lastBtn2 = digitalRead(BTN2);
   lastBtn3 = digitalRead(BTN3);
 
-  // Startup delay to avoid flashing
   delay(2000);
-  while (Serial.available() > 0) Serial.read();  // clear buffer
+  while (Serial.available() > 0) Serial.read();
 }
 
 void loop() {
-  // ----- Button handling -----
   bool btn1 = digitalRead(BTN1);
   bool btn2 = digitalRead(BTN2);
   bool btn3 = digitalRead(BTN3);
@@ -68,14 +61,12 @@ void loop() {
   lastBtn2 = btn2;
   lastBtn3 = btn3;
 
-  // ----- Serial input handling (Inbound signal) -----
   if (Serial.available()) {
       String input = Serial.readStringUntil('\n');
       input.trim();
-      input.toLowerCase();  // make everything lowercase for easy comparison
+      input.toLowerCase();
 
       if (input.length() == 1) {
-          // Toggle single LEDs
           char cmd = input.charAt(0);
           if (cmd == '1') {
               redState = !redState;
